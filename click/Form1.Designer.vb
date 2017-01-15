e20ce5a016e249a16fccd60347ae2819f887f944
JackGrence <green1181772@gmail.com>
2017-01-15 20:16:18 +0800

    backup

---
+++ click/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.MouseD = New System.Windows.Forms.TextBox()
+        Me.MouseC = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(136, 94)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "100"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 123)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(242, 30)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "儲存並關閉視窗"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("微軟正黑體", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(118, 28)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "按住不放熱鍵："
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("微軟正黑體", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 28)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "連點熱鍵："
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("微軟正黑體", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(118, 28)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "連點速度(ms)："
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'MouseD
+        '
+        Me.MouseD.BackColor = System.Drawing.Color.LightGray
+        Me.MouseD.Location = New System.Drawing.Point(136, 38)
+        Me.MouseD.Name = "MouseD"
+        Me.MouseD.ReadOnly = True
+        Me.MouseD.Size = New System.Drawing.Size(100, 23)
+        Me.MouseD.TabIndex = 4
+        '
+        'MouseC
+        '
+        Me.MouseC.BackColor = System.Drawing.Color.LightGray
+        Me.MouseC.Location = New System.Drawing.Point(136, 66)
+        Me.MouseC.Name = "MouseC"
+        Me.MouseC.ReadOnly = True
+        Me.MouseC.Size = New System.Drawing.Size(100, 23)
+        Me.MouseC.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightGray
+        Me.ClientSize = New System.Drawing.Size(273, 163)
+        Me.Controls.Add(Me.MouseC)
+        Me.Controls.Add(Me.MouseD)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Font = New System.Drawing.Font("微軟正黑體", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "點點點"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents MouseD As System.Windows.Forms.TextBox
+    Friend WithEvents MouseC As System.Windows.Forms.TextBox
+
+End Class
---
+++ click/Form1.vb
@@ -0,0 +1,217 @@
+﻿Imports System.Xml, System.IO
+Public Class Form1
+
+    Declare Sub mouse_event Lib "user32" Alias "mouse_event" (ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer, ByVal dwExtraInfo As Integer)
+    Declare Sub GetCursorPos Lib "user32" (ByRef msp As Point)
+    Declare Function GetAsyncKeyState Lib "user32" (ByVal key As Integer) As Short
+    Private Const MOUSEEVENTF_LEFTDOWN = &H2
+    Private Const MOUSEEVENTF_LEFTUP = &H4
+    Private formpos As Point
+    Private mouseDownPos As Point
+    Private mspos As Point
+    Private mousecatch As Boolean = False
+    Private mouseSW As Boolean = False
+    Private mouseUD As Boolean = False
+    Private Dchange As Boolean = False
+    Private Cchange As Boolean = False
+    Private time As Integer = 0
+    Private MD As Integer = 192
+    Private MC As Integer = 20
+    Private MD_KEY As String = "Oemtilde"
+    Private MC_KEY As String = "Capital"
+    Private speed As Integer = 100
+
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        If GetAsyncKeyState(MD) Then 'f10 = 121
+            GetCursorPos(mspos)
+            waitkeyup(MD)
+            If mousecatch Then
+                mousecatch = False
+                mouse_event(MOUSEEVENTF_LEFTUP, mspos.X, mspos.Y, 1, 0)
+                Debug.Print("up")
+            Else
+                mousecatch = True
+                mouse_event(MOUSEEVENTF_LEFTDOWN, mspos.X, mspos.Y, 1, 0)
+                Debug.Print("down")
+            End If
+        ElseIf GetAsyncKeyState(MC) Then
+            GetCursorPos(mspos)
+            waitkeyup(MC)
+            If mouseSW Then
+                mouseSW = False
+                mouseUD = False
+                mouse_event(MOUSEEVENTF_LEFTUP, mspos.X, mspos.Y, 1, 0)
+                Timer2.Enabled = False
+            Else
+                speed = Val(TextBox1.Text)
+                If speed <= 1 Then
+                    TextBox1.Text = 100
+                    speed = 100
+                    MsgBox("別亂來", , "別")
+                    Return
+                End If
+                mouseSW = True
+                mouseUD = True
+                Timer2.Interval = speed / 2
+                Timer2.Enabled = True
+            End If
+        End If
+
+
+
+
+    End Sub
+
+
+    Private Sub Timer2_Tick(sender As System.Object, e As System.EventArgs) Handles Timer2.Tick
+        If mouseUD Then
+            mouse_event(MOUSEEVENTF_LEFTDOWN, mspos.X, mspos.Y, 1, 0)
+        Else
+            mouse_event(MOUSEEVENTF_LEFTUP, mspos.X, mspos.Y, 1, 0)
+        End If
+        mouseUD = Not mouseUD
+        'Debug.Print("mouseUD" & mouseUD)
+    End Sub
+
+    Private Sub cls()
+        For i = 1 To 1000
+            GetAsyncKeyState(121)
+        Next
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        Dim reader As XmlTextReader = New XmlTextReader("config.xml")
+        Dim judge As String
+        If File.Exists("config.xml") Then
+            While (reader.Read())
+                Select Case reader.NodeType
+                    Case XmlNodeType.Element
+                        judge = reader.Name
+                    Case XmlNodeType.Text
+                        Select Case judge
+                            Case "x"
+                                Me.Location = New Point(Int(reader.Value), Me.Location.Y)
+                            Case "y"
+                                Me.Location = New Point(Me.Location.X, Int(reader.Value))
+                            Case "MD"
+                                MD = Int(reader.Value)
+                            Case "MC"
+                                MC = Int(reader.Value)
+                            Case "speed"
+                                speed = Int(reader.Value)
+                            Case "MD_KEY"
+                                MD_KEY = reader.Value
+                            Case "MC_KEY"
+                                MC_KEY = reader.Value
+                        End Select
+                End Select
+
+            End While
+            MouseD.Text = MD_KEY
+            MouseC.Text = MC_KEY
+            TextBox1.Text = speed
+            formpos = New Point(Me.Location)
+            reader.Close()
+        Else
+            creatxml()
+            Form1_Load(sender, e)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        creatxml()
+        Me.Close()
+    End Sub
+
+    Private Sub creatxml()
+        Dim st As String
+        st = _
+        "<?xml version=""1.0""?>" & Environment.NewLine & _
+        "<form>" & Environment.NewLine & _
+        "<pos>" & Environment.NewLine & _
+        "<x>" & Me.Location.X & "</x>" & Environment.NewLine & _
+        "<y>" & Me.Location.Y & "</y>" & Environment.NewLine & _
+        "</pos>" & Environment.NewLine & _
+        "<key>" & Environment.NewLine & _
+        "<MD>" & MD & "</MD>" & Environment.NewLine & _
+        "<MC>" & MC & "</MC>" & Environment.NewLine & _
+        "<MD_KEY>" & MD_KEY & "</MD_KEY>" & Environment.NewLine & _
+        "<MC_KEY>" & MC_KEY & "</MC_KEY>" & Environment.NewLine & _
+        "<speed>" & speed & "</speed>" & Environment.NewLine & _
+        "</key>" & Environment.NewLine & _
+        "</form>"
+        File.WriteAllText("config.xml", st)
+    End Sub
+
+    Private Sub waitkeyup(ByVal k As Integer)
+        While (GetAsyncKeyState(k) <> 0)
+
+        End While
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDown
+        'mouseDownPos = New Point(MousePosition)
+        'Debug.Print(mouseDownPos.X & "  " & mouseDownPos.Y)
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
+        Dim displacement As Point
+        If Me.Capture Then
+            displacement = e.Location - mouseDownPos
+            'Debug.Print("displace" & displacement.ToString)
+            Me.Location = formpos + displacement
+        Else
+            mouseDownPos = e.Location
+            'Debug.Print("downpos" & mouseDownPos.ToString)
+        End If
+        formpos = Me.Location
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseUp
+        'formpos = Me.Location
+    End Sub
+
+    Private Sub MouseD_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles MouseD.KeyDown
+        If Dchange Then
+            MD = e.KeyCode
+            MD_KEY = e.KeyCode.ToString
+            MouseD.Text = MD_KEY
+            Dchange = False
+        End If
+    End Sub
+
+    Private Sub MouseC_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles MouseC.KeyDown
+        If Cchange Then
+            MC = e.KeyCode
+            MC_KEY = e.KeyCode.ToString
+            MouseC.Text = MC_KEY
+            Cchange = False
+        End If
+    End Sub
+
+    Private Sub DCkeyup(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles MouseD.KeyUp, MouseC.KeyUp
+        waitkeyup(MC)
+        waitkeyup(MD)
+        If Dchange = False And Cchange = False Then
+            Timer1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub MouseD_MouseClick(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles MouseD.MouseClick
+
+        MouseD.Text = "請按下熱鍵"
+        Dchange = True
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub MouseC_Click(sender As Object, e As System.EventArgs) Handles MouseC.Click
+
+        MouseC.Text = "請按下熱鍵"
+        Cchange = True
+        Timer1.Enabled = False
+    End Sub
+
+
+End Class
